59b25501dca76d850632b6be4cb1e9eb889c4c7c
kmzakiya <zakyakm@gmail.com>
2022-05-31 17:25:36 +0530

    added test to catch the error in tshirt size

--- tshirts.js
+++ tshirts.js
@@ -11,6 +11,7 @@ function size(cms) {
 }
 
 expect(size(37)).equals('S');
+expect(size(38)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
 console.log('All is well (maybe!)');
