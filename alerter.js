57d66b1584948966aa392913434b0abd1accd1b5
kmzakiya <zakyakm@gmail.com>
2022-05-31 17:32:41 +0530

    added tests

--- alerter.js
+++ alerter.js
@@ -1,26 +1,32 @@
+const { expect } = require("chai");
 let alertFailureCount = 0;
+let threshold = 200;
 
 function networkAlertStub(celcius) {
+  if (celcius > threshold) {
     console.log(`Alert! Temperature is ${celcius} degrees`);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    return 500;
+  }
+  // Return 200 for ok
+  // Return 500 for not-ok
+  // stub always succeeds and returns 200
+  return 200;
 }
 
-function alertInCelcius(farenheit) {
-    const celcius = (farenheit - 32) * 5 / 9;
-    const returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
+function alertInCelcius(farenheit, networkAlert) {
+  const celcius = ((farenheit - 32) * 5) / 9;
+  const returnCode = networkAlert(celcius);
+  if (returnCode != 200) {
+    // non-ok response is not an error! Issues happen in life!
+    // let us keep a count of failures to report
+    // However, this code doesn't count failures!
+    // Add a test below to catch this bug. Alter the stub above, if needed.
+    alertFailureCount += 0;
+  }
 }
 
-alertInCelcius(400.5);
-alertInCelcius(303.6);
+alertInCelcius(400.5, networkAlertStub);
+alertInCelcius(303.6, networkAlertStub);
+expect(alertFailureCount).equals(1);
 console.log(`${alertFailureCount} alerts failed.`);
-console.log('All is well (maybe!)');
+console.log("All is well (maybe!)");
--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,38 @@
-const {expect} = require('chai')
+const { expect } = require("chai");
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
+function get_color_map() {
+  const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+  const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+  let colorMap = [];
+  for (let i = 0; i < majorColors.length; i++) {
+    for (let j = 0; j < minorColors.length; j++) {
+      colorMap.push({
+        code: i * 5 + j,
+        majorColor: majorColors[i],
+        minorColor: minorColors[j],
+      });
     }
-    return majorColors.length * minorColors.length;
+  }
+  return colorMap;
 }
 
-result = print_color_map();
-expect(result).equals(25);
-console.log('All is well (maybe!)');
+function format_color_map(colorMap) {
+  return `${colorMap.code} | ${colorMap.majorColor} | ${colorMap.minorColor}`;
+}
+
+function print_color_map(colorMaps) {
+  colorMaps.forEach((colorMap) => {
+    console.log(format_color_map(colorMap));
+  });
+}
+
+const result = get_color_map();
+print_color_map(result);
+
+expect(result.length).equals(25, "Color map length should be 25");
+expect(result[0].code).equals(1, "Color code should start from 1");
+expect(format_color_map(result[0])).equals(
+  "1 | White | Blue",
+  "First color map should be 1 | White | Blue"
+);
+console.log("All is well (maybe!)");
