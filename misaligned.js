2e352a292b60b608e3fe6ee553c0330caf9e3550
kmzakiya <zakyakm@gmail.com>
2022-06-06 22:20:52 +0530

    alignment correction in color map

--- misaligned.js
+++ misaligned.js
@@ -19,7 +19,7 @@ function get_color_map() {
 }
 
 function format_color_map(colorMap) {
-  return `${colorMap.colorCode} | ${colorMap.majorColor} | ${colorMap.minorColor}`;
+  return `${colorMap.colorCode.toString().padEnd(2,' ')} | ${colorMap.majorColor.padEnd(6,' ')} | ${colorMap.minorColor}`;
 }
 
 function print_color_map(colorMaps) {
@@ -34,7 +34,12 @@ print_color_map(result);
 expect(result.length).equals(25, "Color map length should be 25");
 expect(result[0].colorCode).equals(1, "Color code should start from 1");
 expect(format_color_map(result[0])).equals(
-  "1 | White | Blue",
+  "1  | White  | Blue",
   "First color map should be 1 | White | Blue"
 );
+expect(format_color_map(result[5])).equals(
+  "6  | Red    | Blue"
+);
+expect(format_color_map(result[5]).charAt(3)).equals("|");
+expect(format_color_map(result[5]).charAt(12)).equals("|");
 console.log("All is well (maybe!)");
